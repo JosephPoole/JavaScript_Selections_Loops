dd53935dbdb7509658fb346fbeda518056695a81
Joseph Poole <Joseph.poole90@gmail.com>
2022-08-09 12:50:32 -0500

    all 5 exercises'

--- app.js
+++ app.js
@@ -3,5 +3,106 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+for (let i = 0; 1 <= 100; i++) {
+    if (i % 2 == 0) {
+        continue;
+    }
+    else {
+        consle.log(i);
+
+    }
+}
+
+ // Exercise 2 Section
+ console.log("EXERCISE 2:\n==========\n");
+
+for (let i = 1; i < 100; i++)
+{
+    let output = "";
+    if (i % 3 == 0) {
+        output += "FIZZ"
+    }
+    if (i % 5 == 0) {
+        output += "BUZZ"
+    }
+    console.log(`${i} ${output}`);
+}
+
+ // Exercise 3 Section
+
+let i = 1;
+while (i <= 100)
+{
+    let output = "";
+    if (i % 3 == 0) {
+        output += "FIZZ"
+    }
+    if (i % 5 == 0) {
+        output += "BUZZ"
+    }
+    console.log(`${i} ${output}`);
+
+    i++;
+}
+
+do
+{
+    let output = ""
+
+    if (x % 3 == 0) {
+        output += "FIZZ";
+    }
+
+    if (x % 5 == 0) {
+        output + = "BUZZ";
+    }
+    console.log(`${x} ${output}`);
+
+    x++;
+}
+while (x <= 100);
+
+ // Exercise 4 Section
+
+let value = Math.round((Math.random() * 500));
+        // creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100);
+        // creates a random number between 100 and 500
+
+for ( let i = 1; i <= n; i++)
+{
+    if (i == value)
+    {
+        console.log(`Found ${value}.`);
+        break;
+    }
+
+    if (i == n)
+    {
+        console.log(`did not find ${value} within 1 through ${n}.`)
+    }
+}
+
+  // Exercise 5 Section
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let end = Math.round(Math.random() * (1000 - 1) + 1);
+let start = Math.round(Math.random() * (10 - 1) + 1);
+
+for (let i = start; i <= end; i++)
+{
+    if (i % fizzDivisor == 0)
+    {
+        output += "FIZZ";
+    }
+
+    if (i % buzzDivisor == 0)
+    {
+        output += "BUZZ";
+    }
+
+    console.log(`${i} ${output}`);
+}
+
+console.log(fizzDivisor, buzzDivisor);
+
